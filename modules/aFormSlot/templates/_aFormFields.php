fd6204a95fcb5226ef887dc6cc6b17e92c0268d6
agilbert <agilbert@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-01-27 16:09:56 +0000

    Fixing the modules.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheFormBuilderPlugin/trunk@80 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aFormSlot/templates/_aFormFields.php
+++
@@ -1,5 +0,0 @@
-<?php foreach ($a_form->getAllFieldsByRank() as $field): ?>
-<ul id="a-form-field-<?php echo $field->getId() ?>" class="a-form-row">
-<?php echo include_partial('aForm/aFormFieldEdit', array('a_form_field' => $field)); ?>
-</ul>
-<?php endforeach ?>
