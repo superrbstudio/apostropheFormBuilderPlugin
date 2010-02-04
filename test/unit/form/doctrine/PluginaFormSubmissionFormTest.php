fe7425888b58a6fd0fbc99d88048eb991de2ac78
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-04 20:52:52 +0000

    Test fixture update, to work with existing form fields, and some initial tests.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheFormBuilderPlugin/trunk@155 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- test/unit/form/doctrine/PluginaFormSubmissionFormTest.php
+++ test/unit/form/doctrine/PluginaFormSubmissionFormTest.php
@@ -5,8 +5,22 @@
  */
 include dirname(__FILE__).'/../../../bootstrap/Doctrine.php';
 
-$t = new lime_test(0);
+$t = new lime_test(2);
 
+$a_form = Doctrine::getTable('aForm')->createQuery()->fetchOne();
 
-$form = new aFormSubmissionForm();
+$form = new aFormBuilder(array(), array('a_form' => $a_form));
 
+/*
+ * Tests for form creation
+ */
+$t->is(count($form->getEmbeddedForm('fields')->getEmbeddedForms()), count($a_form->aFormFields), 'Correct number of field forms were embedded');
+
+$bool = true;
+foreach($a_form->aFormFields as $field)
+{
+  if(!$field->getType() == $form->getEmbeddedForm('fields')->getEmbeddedForm($field->getId()))
+    $bool = false;
+}
+
+$t->ok($bool, "Each form is embedded with proper field type.");
