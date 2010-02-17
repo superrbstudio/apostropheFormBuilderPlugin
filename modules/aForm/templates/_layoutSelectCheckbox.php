47e16823f22183a2dfc1bfd9be90e63b30a97d02
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-17 21:42:08 +0000

    Static markup exampels of the checkbox and radio edit views
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheFormBuilderPlugin/trunk@393 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aForm/templates/_layoutSelectCheckbox.php
+++ modules/aForm/templates/_layoutSelectCheckbox.php
@@ -1,1 +1,13 @@
-
+<li class="options-label"><label><?php echo $aFormLayout['label'] ?></label></li>
+<li class="options">
+	<div class="options-container">
+		<ul>
+			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 1</label> <a href="#" class="a-btn icon icon-only a-delete" Title="Delete">Delete</a></li>
+			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 2</label> <a href="#" class="a-btn icon icon-only a-delete" Title="Delete">Delete</a></li>
+			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 3</label> <a href="#" class="a-btn icon icon-only a-delete" Title="Delete">Delete</a></li>
+			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 4</label> <a href="#" class="a-btn icon icon-only a-delete" Title="Delete">Delete</a></li>
+			<li class="option new"><input type="checkbox" disabled="disabled" /> <label><input type="text" value="New Option" class="a-default-value" /></label> <a href="#" class="a-btn icon icon-only a-add" title="Add Option">Add</a></li>
+		</ul>
+	</div>
+	<button type="submit" class="a-btn a-submit">Save Options</button>
+</li>
