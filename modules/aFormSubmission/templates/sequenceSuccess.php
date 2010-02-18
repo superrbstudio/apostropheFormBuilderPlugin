c33dfbdc670341f5c5b9b9cc9f2a12c89beb5121
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-18 20:31:13 +0000

    Fixed change to the way partial was being included in sequenceSuccess.php
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheFormBuilderPlugin/trunk@418 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aFormSubmission/templates/sequenceSuccess.php
+++ modules/aFormSubmission/templates/sequenceSuccess.php
@@ -3,7 +3,8 @@
 <?php echo form_tag('@a_form_submission_sequence?id='.$aFormSubmission->getId().'&form_id='.$aForm->getId().'&layout_rank='.($aFormLayout->getRank())); ?>
 
 <ul class="a-form-field <?php echo $aFormLayout->getType() ?>">
-<?php include_partial('aForm/layout'.sfInflector::camelize($aFormLayout->getType()), array('aFormLayout' => $aFormLayout, 'form' => $aFormLayout->getForm(), 'disabled' => false)) ?>
+<?php include_partial('aForm/layout'.sfInflector::camelize($aFormLayout->getType()), array('aFormLayout' => $aFormLayout, 'form' => $form, 'disabled' => false)) ?>
+<?php echo $form->renderHiddenFields() ?>
 </ul>
 
 <input type="submit" ?>
