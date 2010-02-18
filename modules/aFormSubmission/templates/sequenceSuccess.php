3c5bdd9672eb7c83c012bfe267ee08a035b5bb76
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-18 20:42:33 +0000

    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheFormBuilderPlugin/trunk@419 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aFormSubmission/templates/sequenceSuccess.php
+++ modules/aFormSubmission/templates/sequenceSuccess.php
@@ -1,4 +1,8 @@
-<?php echo $pos ?> of <?php echo count($aForm->aFormLayouts) ?>
+<?php slot('body_class','a-form-builder filling-out') ?>
+
+<?php slot('a-page-title', '<span>Form Builder :</span> Filling Out Survey') ?>
+
+<h4><span><em>Question <?php echo $pos ?> </em> of <?php echo count($aForm->aFormLayouts) ?></span></h4>
 
 <?php echo form_tag('@a_form_submission_sequence?id='.$aFormSubmission->getId().'&form_id='.$aForm->getId().'&layout_rank='.($aFormLayout->getRank())); ?>
 
@@ -7,4 +11,4 @@
 <?php echo $form->renderHiddenFields() ?>
 </ul>
 
-<input type="submit" ?>
+<input type="submit" class="a-submit" name="submit" />
