52d1b8dcecfe514545d4f2651365efe65560c7fe
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-23 16:41:37 +0000

    Fixed doctrine routing issue for new form submission sequences.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheFormBuilderPlugin/trunk@441 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aForm/templates/indexSuccess.php
+++ modules/aForm/templates/indexSuccess.php
@@ -12,7 +12,7 @@
 	  <ul class="a-form-list-item">
 	  	<li class="title"><h5><?php echo link_to($aForm->getName(), 'a_form_edit', $aForm) ?></h5></li>
 	  	<li><?php echo link_to('Edit this form', 'a_form_edit', $aForm, array('class' => 'a-btn icon icon-only a-edit', )) ?></li>
-	  	<li><?php echo link_to('Fill out this form', '@a_form_submission_sequence_new?form_id='. $aForm->getId(), array('class' => 'a-btn', )) ?></li>
+	  	<li><?php echo link_to('Fill out this form', '@a_form_submission_sequence_new?id='. $aForm->getId(), array('class' => 'a-btn', )) ?></li>
       <li><?php echo link_to('See the data', '@a_form_submission_admin?form_id='. $aForm->getId(), array('class' => 'a-btn')) ?></li>
 	  </ul>
 	</li>
