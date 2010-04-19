ed3eaf3def7293b292ba3f92cb4b7dcf77622bad
rickybanister <rickybanister@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-19 19:42:14 +0000

    changed old 'icon-only' selectors to new 'no-label'
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheFormBuilderPlugin/trunk@979 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aForm/templates/_aFormFieldsetEdit.php
+++ modules/aForm/templates/_aFormFieldsetEdit.php
@@ -1,17 +1,17 @@
 <?php use_helper('jQuery') ?>
 
-<li class="drag"><?php echo jq_link_to_function('Drag', '', array('class' => 'a-btn icon icon-only a-drag nobg alt', 'title' => 'Drag to Order',  )) ?></li>
+<li class="drag"><?php echo jq_link_to_function('Drag', '', array('class' => 'a-btn icon no-label a-drag nobg alt', 'title' => 'Drag to Order',  )) ?></li>
 <li class="delete"><?php echo jq_link_to_remote('Delete', array(
   'url' => '@a_form_deleteFieldset?id='.$aForm->getId().'&fieldset_id='.$aFormFieldset->getId(),
   'confirm' => 'Are you sure you want to delete this field? If you do, you will no longer be able to retrieve the data from this column.',  
   'complete' => '$("#a-form-fieldset-'.$aFormFieldset->getId().'").hide()', 
   'method' => 'post', 
-), array('class' => 'a-btn icon icon-only a-delete', 'title' => 'Delete', )) ?></li>
+), array('class' => 'a-btn icon no-label a-delete', 'title' => 'Delete', )) ?></li>
 <li class="edit"><?php echo jq_link_to_remote('Edit<span></span>', array(
   'update' => 'a-form-fieldset-'.$aFormFieldset->getId(),
   'url' => '@a_form_editFieldset?id='.$aFormFieldset->getFormId().'&fieldset_id='.$aFormFieldset->getId(),
   'script' => true, 
-), array('class' => 'a-btn icon icon-only a-edit', 'title' => 'Edit', )) ?></li>
+), array('class' => 'a-btn icon no-label a-edit', 'title' => 'Edit', )) ?></li>
 
 <li>
 	<ul class="a-form-field <?php echo $aFormFieldset->getType() ?>">
--- modules/aForm/templates/_fieldsetSelectCheckbox.php
+++ modules/aForm/templates/_fieldsetSelectCheckbox.php
@@ -2,11 +2,11 @@
 <li class="options">
 	<div class="options-container">
 		<ul>
-			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 1</label> <span>Some Value</span> <a href="#" class="a-btn icon icon-only a-delete" Title="Delete">Delete</a></li>
-			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 2</label> <span>Some Value</span> <a href="#" class="a-btn icon icon-only a-delete" Title="Delete">Delete</a></li>
-			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 3</label> <span>Some Value</span> <a href="#" class="a-btn icon icon-only a-delete" Title="Delete">Delete</a></li>
-			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 4</label> <span>Some Value</span> <a href="#" class="a-btn icon icon-only a-delete" Title="Delete">Delete</a></li>
-			<li class="option new"><input type="checkbox" disabled="disabled" /><label><input type="text" value="Label" class="a-default-value" /></label><input type="text" value="Value" class="a-default-value" /><a href="#" class="a-btn icon icon-only a-add" title="Add Option">Add</a></li>
+			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 1</label> <span>Some Value</span> <a href="#" class="a-btn icon no-label a-delete" Title="Delete">Delete</a></li>
+			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 2</label> <span>Some Value</span> <a href="#" class="a-btn icon no-label a-delete" Title="Delete">Delete</a></li>
+			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 3</label> <span>Some Value</span> <a href="#" class="a-btn icon no-label a-delete" Title="Delete">Delete</a></li>
+			<li class="option"><input type="checkbox" disabled="disabled" /> <label>Option 4</label> <span>Some Value</span> <a href="#" class="a-btn icon no-label a-delete" Title="Delete">Delete</a></li>
+			<li class="option new"><input type="checkbox" disabled="disabled" /><label><input type="text" value="Label" class="a-default-value" /></label><input type="text" value="Value" class="a-default-value" /><a href="#" class="a-btn icon no-label a-add" title="Add Option">Add</a></li>
 		</ul>
 	</div>
 	<button type="submit" class="a-btn a-submit">Save Options</button>
