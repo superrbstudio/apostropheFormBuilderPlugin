ab22c847defe25457af9e662ba352e67aa13403f
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-23 21:40:52 +0000

    Form builder plugin migrations for moving from pkFormBuilderPlugin (I need to verify that the end results are good, so far they look good)
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheFormBuilderPlugin/trunk@2714 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- config/apostropheFormBuilderPluginConfiguration.class.php
+++ config/apostropheFormBuilderPluginConfiguration.class.php
@@ -12,10 +12,75 @@ class apostropheFormBuilderPluginConfiguration extends sfPluginConfiguration
 {
   const VERSION = '1.0.0-DEV';
 
+  static $registered = false;
   /**
    * @see sfPluginConfiguration
    */
   public function initialize()
   {
+    // Yes, this can get called twice. This is Fabien's workaround:
+    // http://trac.symfony-project.org/ticket/8026
+    
+    if (!self::$registered)
+    {
+      // Hook up to apostrophe:migrate
+      $this->dispatcher->connect('command.post_command', array('apostropheFormBuilderPluginConfiguration',  
+        'listenToCommandPostCommandEvent'));
+      
+      self::$registered = true;
+    }
+  }
+  
+  static public function migrate()
+  {
+    $migrate = new aMigrate(Doctrine_Manager::connection()->getDbh());
+    echo("Migrating apostropheFormBuilderPlugin...\n");
+    if ((!$migrate->tableExists('a_form')) && ($migrate->tableExists('pk_form')))
+    {
+      $migrate->sql(array(
+        'RENAME TABLE pk_form TO a_form',
+        // Yes really, a new layer was added
+        'RENAME TABLE pk_form_field TO a_form_fieldset',
+        // TODO: field_id has to become fieldset_id, and field_id has to get added and
+        // reference the new a_form_field object which has to be created
+        'RENAME TABLE pk_form_field_submission to a_form_field_submission',
+        // field_id has to become fieldset_id
+        'RENAME TABLE pk_form_field_option to a_form_fieldset_option',
+        'RENAME TABLE pk_form_submission to a_form_submission',
+        'ALTER TABLE a_form_field_submission CHANGE field_id fieldset_id BIGINT',
+        'ALTER TABLE a_form_field_submission ADD COLUMN field_id BIGINT',
+        'ALTER TABLE a_form_fieldset_option CHANGE field_id fieldset_id BIGINT',
+        'CREATE TABLE a_form_field (id BIGINT AUTO_INCREMENT, fieldset_id BIGINT, name TEXT, INDEX fieldset_id_idx (fieldset_id), PRIMARY KEY(id)) ENGINE = INNODB'
+      ));
+      $fieldsets = $migrate->query('SELECT * FROM a_form_fieldset');
+      foreach ($fieldsets as $fieldset)
+      {
+        $migrate->query('INSERT INTO a_form_field (fieldset_id, name) VALUES (:fieldset_id, :name)', array('fieldset_id' => $fieldset['id'], 'name' => 'value'));
+        $id = $migrate->lastInsertId();
+        $migrate->query('UPDATE a_form_field_submission SET field_id = :field_id WHERE fieldset_id = :fieldset_id', array('field_id' => $id, 'fieldset_id' => $fieldset['id']));
+      }
+    }
+
+    if (!$migrate->getCommandsRun())
+    {
+      echo("Your database is already up to date.\n\n");
+    }
+    else
+    {
+      echo($migrate->getCommandsRun() . " SQL commands were run.\n\n");
+    }
+    echo("Done!\n");
+    
+  }
+  
+  // command.post_command
+  static public function listenToCommandPostCommandEvent(sfEvent $event)
+  {
+    $task = $event->getSubject();
+    
+    if ($task->getFullName() === 'apostrophe:migrate')
+    {
+      self::migrate();
+    }
   }
 }
